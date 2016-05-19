60b23ca5f9d44a0f809ff6ad5a7ee70c9a9c50e2
karaframe <karafede75@gmail.com>
2016-05-19 15:26:11 +0100

    Add files via upload

---
+++ OpenLR_R_scripts/OpenLR_FK_19_May_2016_all.R
@@ -0,0 +1,178 @@
+
+library(maptools)
+library(threadr) # load this package before dplyr
+library(leaflet)
+library(dplyr)
+library(rgdal)
+library(raster)
+library(rgeos)
+library(importr)
+library(lubridate)
+library(webshot)
+library(htmlwidgets)
+
+# setwd("C:/RICARDO-AEA/PostgreSQL_Gibraltair/OpenLR")
+ setwd("C:/PostgreSQL_Gibraltair/OpenLR")
+
+# all data for 05 may 2016
+# traffic <- read.csv("traffic_flow_5_May_2016.csv") 
+traffic_1 <- read.csv("traffic_flow_FK_18May2016_5pm.csv")
+traffic_2 <- read.csv("traffic_flow_FK_18May2016_23pm.csv")
+
+#########################################################################################
+#########################################################################################
+
+# POINTS_1st <- read.csv("points_traffic_flow_1st_05_May.csv")[2:4]
+# POINTS_2nd <- read.csv("points_traffic_flow_2nd_05_May.csv")[2:4]
+
+POINTS_1st_1 <- read.csv("points_traffic_flow_1st_18May2016_5pm.csv")[2:4]
+POINTS_2nd_1 <- read.csv("points_traffic_flow_2nd_18May2016_5pm.csv")[2:4]
+
+POINTS_1st_2 <- read.csv("points_traffic_flow_1st_18May2016_23pm.csv")[2:4]
+POINTS_2nd_2 <- read.csv("points_traffic_flow_2nd_18May2016_23pm.csv")[2:4]
+
+
+# Join traffic data (speed) first and second point
+traffic_1st_1 <- cbind(traffic_1,POINTS_1st_1)
+traffic_2nd_1 <- cbind(traffic_1,POINTS_2nd_1)
+
+traffic_1st_2 <- cbind(traffic_2,POINTS_1st_2)
+traffic_2nd_2 <- cbind(traffic_2,POINTS_2nd_2)
+
+# bind 1st and 2nd point together
+traffic <- rbind(traffic_1st_1, traffic_2nd_1, traffic_1st_2, traffic_2nd_2)
+# write.csv(traffic, "traffic_realtime_05May2016.csv")
+# traffic <- read.csv("traffic_realtime_05May2016.csv")
+write.csv(traffic, "traffic_realtime_18_19May2016.csv")
+traffic <- read.csv("traffic_realtime_18_19May2016.csv")
+
+
+# pair of points
+# POINTS <- read.csv("points_traffic_flow_05_May.csv")
+# Join traffic data (speed) to spatial data
+# traffic <- cbind(traffic,POINTS)
+
+
+# remove duplicates of lon & lat and sort latitude from big to small
+traffic <- traffic %>%
+  distinct(lat) 
+# remove traffic data with latitude == 36.1470043657 (point in the water)
+traffic <- traffic[traffic$lat!=36.1470043657487,]   
+# remove traffic data with latitude == 36.1470043657 (point before the border)
+traffic <- traffic[traffic$lat!=36.1560165880387,]   
+traffic <- traffic[traffic$lat!=36.147002876331,]  
+
+# traffic <- traffic[traffic$averagespeed!=0,] 
+
+ traffic <- traffic %>%
+   arrange(lat) %>%
+   arrange(lon)
+
+# load open street data (routes)
+#  dir <-  "C:/RICARDO-AEA/postgreSQL_Gibraltair"
+#  OSM_GIB <- readOGR(dsn = dir, layer = "gibraltar_OSM")
+# read GeoJSON-------------------------------------------------------------
+OSM_GIB <- readOGR(".GIB_geojson_Open_Street", "OGRGeoJSON")
+plot(OSM_GIB)
+  
+# # make polygon
+# p1 = Polygon(traffic[,22:23]) #lon & lat
+# # make Polygon class
+# p2 = Polygons(list(p1), ID = "drivetime")
+# # make spatial polygons class
+# p3 = SpatialPolygons(list(p2),proj4string=CRS("+init=epsg:4326"))
+# plot(p3)
+ 
+ 
+# make a spatial dataframe with traffic data 
+sp_traffic <- SpatialPointsDataFrame(traffic[,22:23], traffic,            # lat, lon
+                                            proj4string=CRS("+init=epsg:4326")) 
+plot(sp_traffic)
+  
+
+# make lines
+p1 = Line(traffic[,22:23]) #lon & lat
+# make Polygon class
+p2 = Lines(list(p1), ID = "drivetime")
+# make spatial polygons class
+traffic_lines = SpatialLines(list(p2),proj4string=CRS("+init=epsg:4326"))
+plot(traffic_lines)
+
+
+#########################################################################################
+# cut routes between points--------------------------
+
+# gI <- rgeos::gIntersection(OSM_GIB, traffic_lines, byid=c(TRUE, TRUE))
+# plot(gI)
+# plot(gI, col=1:4, lwd=2, add=TRUE)
+# row.names(gI)
+# Inter <- as.data.frame(gI)
+# colnames(Inter) <- c("Lon", "Lat")
+
+
+buffer_sp_traffic <- rgeos::gBuffer(sp_traffic, width=0.00015)  #0.00005
+plot(sp_traffic)
+plot(buffer_sp_traffic, add = TRUE)
+gI <- gIntersection(OSM_GIB, buffer_sp_traffic,  byid=c(TRUE, TRUE))
+# check_intersect_points <- rgeos::gCrosses(OSM_GIB, buffer_sp_traffic,  byid=c(TRUE, TRUE))
+plot(gI)
+plot(gI, col=1:4, lwd=2, add=TRUE)
+
+
+#########################################################################################
+
+# make a quick leaflet map with site loaction only
+
+
+MAX_speed <- max(traffic$averagespeed)+0.5  
+MIN_speed <- min(traffic$averagespeed)-0.5
+
+pal_speed <- colorNumeric(c("#0000ff", "#ff0000"),
+                          c(MIN_speed, MAX_speed),na.color = "transparent")
+
+# define popup for the iteractive map
+popup_speed <- paste0("<p>Avg Speed", ": <strong> ", traffic$averagespeed, " </strong>km/h")
+
+
+
+# Marker + lines + Static Label using custom label options
+map <- leaflet(data = traffic[,]) %>%
+  setView(-5.35, 36.150, 16) %>%
+  addTiles(group = "OSM (default)") %>%
+  addProviderTiles("OpenStreetMap.Mapnik", group = "Road map") %>%
+  addProviderTiles("Thunderforest.Landscape", group = "Topographical") %>%
+  addProviderTiles("Esri.WorldImagery", group = "Satellite") %>%
+  addProviderTiles("Stamen.TonerLite", group = "Toner Lite") %>%
+  addCircleMarkers(lng = ~lon, lat = ~lat, 
+                  # weight = 1, radius=5, color = 'black',
+                  # stroke = FALSE, fillOpacity = 1,
+                   popup = popup_speed,
+                   weight = 3, radius= 4, 
+                   color = pal_speed(traffic$averagespeed), stroke = FALSE, fillOpacity = 1,
+                  label = ~as.character(traffic$informationstatus),
+                  labelOptions = labelOptions(noHide = F),
+                   group = "speed (km/h)") %>%
+  addPolylines(data = traffic_lines, color='blue', group='Route') %>%
+  addPolylines(data = OSM_GIB , color='blue', group='OSM', weight = 1) %>%
+#   addCircleMarkers(lng = Inter$Lon, lat = Inter$Lat, 
+#                    weight = 1, radius=5, color = 'black',
+#                    stroke = FALSE, fillOpacity = 1,
+#                    group = "intersects") %>%
+  addPolylines(data = gI, color="blue", group='intersects', weight = 2,
+               label = as.character(traffic$informationstatus),
+               labelOptions = labelOptions(noHide = F)) %>%
+  addLegend(
+    "bottomright", pal = pal_speed, values = c(MIN_speed, MAX_speed),
+    title = paste("<strong>Avg Speed Km/h:"),
+    labFormat = labelFormat(prefix = ""), labels = "black", opacity = 1) %>%
+  addLayersControl(
+    baseGroups = c("Road map", "Topographical", "Satellite", "Toner Lite"),
+    overlayGroups = c("speed (km/h)", "Route", "OSM", "intersects"),
+    options = layersControlOptions(collapsed = TRUE)) %>%
+    hideGroup(c("OSM", "Route","intersects"))
+
+map
+
+
+
+
--- OpenLR_R_scripts/Traffic_data_all.R
+++ OpenLR_R_scripts/Traffic_data_all.R
@@ -14,18 +14,20 @@ library(htmlwidgets)
 options(warn=-1) # warnings OFF
 # options(warn=0)  # warnings back ON!
 
-setwd("C:/RICARDO-AEA/PostgreSQL_Gibraltair/OpenLR")
-# setwd("C:/PostgreSQL_Gibraltair/OpenLR")
+# setwd("C:/RICARDO-AEA/PostgreSQL_Gibraltair/OpenLR")
+setwd("C:/PostgreSQL_Gibraltair/OpenLR")
 
 # load traffic data until 11 May 2016
 # these are stationary Traffic & queueing Traffic & low Traffic
-traffic <- read.csv("traffic_data_FK_until_11_May_2016.csv")  
+# traffic <- read.csv("traffic_data_FK_until_11_May_2016.csv")  
+traffic <- read.csv("traffic_data_FK_until_19_May_2016.csv")  
 
 
 # load shp file for open street data (routes)
-dir <-  "C:/RICARDO-AEA/postgreSQL_Gibraltair/OpenLR"
-OSM_GIB <- readOGR(dsn = dir, layer = "gibraltar_OSM")
-plot(OSM_GIB)
+# dir <-  "C:/RICARDO-AEA/postgreSQL_Gibraltair/OpenLR"
+# dir <-  "C:/postgreSQL_Gibraltair/OpenLR"
+# OSM_GIB <- readOGR(dsn = dir, layer = "gibraltar_OSM")
+# plot(OSM_GIB)
 
 #### Write GeoJSON file for OSM_GIB ############################
 # ----- Write data to GeoJSON
@@ -37,22 +39,25 @@ writeOGR(OSM_GIB, dat, layer="", driver="GeoJSON")
 
 # read GeoJSON-------------------------------------------------------------
 OSM_GIB <- readOGR(".GIB_geojson_Open_Street", "OGRGeoJSON")
-
+plot(OSM_GIB)
 
 ###################################################################################################
 
-POINTS_1st <- read.csv("points_traffic_data_1st_11_May.csv")[2:4]
-POINTS_2nd <- read.csv("points_traffic_data_2nd_11_May.csv")[2:4]
+# POINTS_1st <- read.csv("points_traffic_data_1st_11_May.csv")[2:4]
+# POINTS_2nd <- read.csv("points_traffic_data_2nd_11_May.csv")[2:4]
 
-# Join traffic data (speed) to spatial data
+POINTS_1st <- read.csv("points_traffic_data_1st_19May2016.csv")[2:4]
+POINTS_2nd <- read.csv("points_traffic_data_2nd_19May2016.csv")[2:4]
+
+# Join traffic data (speed) first and second point
 # traffic <- cbind(traffic,POINTS)
 traffic_1st <- cbind(traffic,POINTS_1st)
 traffic_2nd <- cbind(traffic,POINTS_2nd)
 
 # bind 1st and 2nd point together
 traffic <- rbind(traffic_1st, traffic_2nd)
-write.csv(traffic, "traffic_stationary_until_11May2016.csv")
-traffic <- read.csv("traffic_stationary_until_11May2016.csv")
+write.csv(traffic, "traffic_stationary_until_19May2016.csv")
+traffic <- read.csv("traffic_stationary_until_19May2016.csv")
 
 
 # remove duplicates of lon & lat and sort latitude from big to small
@@ -62,6 +67,7 @@ traffic <- traffic %>%
 traffic <- traffic[traffic$lat!=36.1470043657487,]   
 # remove traffic data with latitude == 36.1470043657 (point before the border)
 traffic <- traffic[traffic$lat!=36.1560165880387,]   
+traffic <- traffic[traffic$averagespeed!=0,] 
 
 
 #########################################################################################
@@ -82,6 +88,17 @@ gI <- gIntersection(OSM_GIB, buffer_sp_traffic,  byid=c(TRUE, TRUE))
 plot(gI)
 plot(gI, col=1:4, lwd=2, add=TRUE)
 
+#######################################################################################
+
+MAX_speed <- max(traffic$averagespeed)+0.5  
+MIN_speed <- min(traffic$averagespeed)-0.5
+
+pal_speed <- colorNumeric(c("#0000ff", "#ff0000"),
+                          c(MIN_speed, MAX_speed),na.color = "transparent")
+
+# define popup for the iteractive map
+popup_speed <- paste0("<p>Avg Speed", ": <strong> ", traffic$averagespeed, " </strong>km/h")
+
 
 # make a quick leaflet map with site loaction only
 
@@ -93,19 +110,25 @@ map <- leaflet(data = traffic[,]) %>%
   addProviderTiles("Esri.WorldImagery", group = "Satellite") %>%
   addProviderTiles("Stamen.TonerLite", group = "Toner Lite") %>%
   addCircleMarkers(lng = ~lon, lat = ~lat, 
-                   weight = 1, radius=5, color = 'black',
-                   stroke = FALSE, fillOpacity = 1,
+#                    weight = 1, radius=5, color = 'black',
+#                    stroke = FALSE, fillOpacity = 1,
+                   popup = popup_speed,
+                   weight = 3, radius= 4, 
+                   color = pal_speed(traffic$averagespeed), stroke = FALSE, fillOpacity = 1,
                    label = ~as.character(traffic$abnormaltraffictype),
                    labelOptions = labelOptions(noHide = F),
                    group = "traffic") %>%
-  addPolylines(data = gI, color='blue', group='Route',
+  addPolylines(data = gI, color='red', group='Route',
                label = as.character(traffic$abnormaltraffictype),
                labelOptions = labelOptions(noHide = F)) %>%
+  addLegend("bottomright", pal = pal_speed, values = c(MIN_speed, MAX_speed),
+    title = paste("<strong>Avg Speed Km/h:"),
+    labFormat = labelFormat(prefix = ""), labels = "black", opacity = 1) %>%
 addLayersControl(
   baseGroups = c("Road map", "Topographical", "Satellite", "Toner Lite"),
   overlayGroups = c("traffic", "Route"),
   options = layersControlOptions(collapsed = TRUE)) %>%
-  hideGroup(c("traffic"))
+  hideGroup(c("Route"))
 
 map
 
@@ -114,13 +137,8 @@ map
 
 
 
-
-
-
-
-
 ## create a series of .png figures for each hour
-saveWidget(map, 'gibraltar_TomTom_Stationary.html', selfcontained = FALSE)
+# saveWidget(map, 'gibraltar_TomTom_Stationary.html', selfcontained = FALSE)
 
 
 ###################### end #############################################################
